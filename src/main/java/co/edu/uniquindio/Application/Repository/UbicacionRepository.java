2676c4103c1c9f0992bf1df87dc9ff9b36bef395
Cel18 <celeste.buitragor@uqvirtual.edu.co>
2025-09-25 20:40:59 -0500

    Repositories pt.1 - corregí una cosita - me falto esto nopuedeser

--- src/main/java/co/edu/uniquindio/Application/Repository/UbicacionRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uniquindio.Application.Repository;
-
-import co.edu.uniquindio.Application.Model.Ubicacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UbicacionRepository extends JpaRepository<Ubicacion, Long> {
-}
